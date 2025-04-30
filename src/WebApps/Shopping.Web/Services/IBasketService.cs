2a3b3662ecbdf8602bdc02dcf4d5b63d11508223
GustavoNogueira <gustavonogueira2005@gmail.com>
2025-04-30 16:00:27 +0100

    Section 20: Frontend with Razor Pages and Refit

---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,40 @@
+﻿namespace Shopping.Web.Services
+{
+    public interface IBasketService
+    {
+        [Get("/basket-service/basket/{username}")]
+        Task<GetBasketResponse> GetBasket(string username);
+
+        [Post("/basket-service/basket")]
+        Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+        [Post("/basket-service/basket/checkout")]
+        Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+        [Delete("/basket-service/basket/checkout")]
+        Task<DeleteBasketResponse> DeletBasket(string username);
+
+        public async Task<ShoppingCartModel> LoadUserBasket()
+        {
+            // Get Basket, If not existe create new basket with default logged in user name
+            var username = "swn";
+            ShoppingCartModel basket;
+
+            try
+            {
+                var getBasketResponse = await GetBasket(username);
+                basket = getBasketResponse.Cart;
+            }
+            catch (Exception)
+            {
+                basket = new ShoppingCartModel
+                {
+                    Username = username,
+                    Items = []
+                };
+            }
+
+            return basket;
+        }
+    }
+}
